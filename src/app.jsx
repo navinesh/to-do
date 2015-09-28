b794cf921db13dd0c375a68113fd7c601da915f3
navinesh <navinesh.chand@gmail.com>
2015-09-28 17:32:13 +1200

    sync src folder

--- src/app.jsx
+++ src/app.jsx
@@ -2,12 +2,21 @@ var React = require('react');
 var Firebase = require('firebase');
 var ReactFire = require('reactfire');
 var Header = require('./header');
+var List = require('./list');
 var rootURL = 'https://resplendent-heat-3459.firebaseio.com/';
 
 var app = React.createClass({
   mixins: [ ReactFire ],
+  getInitialState: function(){
+    return {
+      items: {},
+      loaded: false
+    }
+  },
   componentWillMount: function(){
-      this.bindAsObject(new Firebase(rootURL + 'items/'), 'items');
+      fb = new Firebase(rootURL + 'items/');
+      this.bindAsObject(fb, 'items');
+      fb.on('value', this.handleDataLoaded);
     },
   render: function() {
     return <div className="row panel panel-default">
@@ -15,9 +24,15 @@ var app = React.createClass({
         <h2 className="text-center">
           Todo List
         </h2>
-        <Header />
+        <Header itemsStore={this.firebaseRefs.items} />
+        <div className={"content " + (this.state.loaded ? 'loaded' : '')}>
+          <List items={this.state.items} />
+        </div>
       </div>
     </div>
+  },
+  handleDataLoaded: function(){
+    this.setState({loaded: true});
   }
 });
 
